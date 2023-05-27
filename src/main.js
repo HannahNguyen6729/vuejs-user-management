5b5fbaf92d545e87f5e11a4cf61a8b1b1efe89a8
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-05-27 16:20:33 +0300

    create a store to store user list

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createStore } from "vuex";
+import userList from "../src/data/devs.json";
 
 import AppModal from "./components/AppModal.vue";
+
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+      userList,
+    };
+  },
+});
+
 const app = createApp(App);
+
+// Install the store instance as a plugin
+app.use(store);
 
 app.component("app-modal", AppModal);
 
